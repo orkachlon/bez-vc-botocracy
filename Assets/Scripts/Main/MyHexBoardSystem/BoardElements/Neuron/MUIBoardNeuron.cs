3dc3a93d7c5bc2b1792288c238cfded943e65eae
Or Kachlon <or.kachlon@mail.huji.ac.il>
2023-06-11 07:48:10 +0300

    Made hover neuron slightly bigger

--- Assets/Scripts/Main/MyHexBoardSystem/BoardElements/Neuron/MUIBoardNeuron.cs
+++ Assets/Scripts/Main/MyHexBoardSystem/BoardElements/Neuron/MUIBoardNeuron.cs
@@ -1,15 +1,17 @@
 ﻿using ExternBoardSystem.Ui.Board;
+using UnityEngine;
 
 namespace Main.MyHexBoardSystem.BoardElements.Neuron {
     public class MUIBoardNeuron : MUIBoardElement {
-
-
+        
         public void ToFront() {
             SpriteRenderer.sortingOrder = 1;
+            transform.localScale *= 1.2f;
         }
 
         public void ToBack() {
             SpriteRenderer.sortingOrder = 0;
+            transform.localScale = Vector3.one;
         }
     }
 }
